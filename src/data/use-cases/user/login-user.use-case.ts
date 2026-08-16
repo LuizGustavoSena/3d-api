967a2675facee9e201b4dbea7635747d9ee1de33
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-08-16 19:59:05 -0300

    feat: LoginUserUseCase class

--- src/data/use-cases/user/login-user.use-case.ts
+++ src/data/use-cases/user/login-user.use-case.ts
@@ -2,15 +2,19 @@ import { UnauthorizedUserError } from "../../../domain/errors/user/unauthorized-
 import { IUsecase } from "../../../domain/use-cases/use-cases.interface";
 import { RequestLoginUser, ResponseLoginUser } from "../../../domain/use-cases/user/login-user.interface";
 import { IEncrypt } from "../../protocols/encrypt";
+import { IJwt } from "../../protocols/jwt";
 import { IUserRepository } from "../../protocols/repositories/user";
+import { IUuid } from "../../protocols/uuid";
 
 export class LoginUserUseCase implements IUsecase<RequestLoginUser, ResponseLoginUser> {
     constructor(
         private readonly userRepository: IUserRepository,
         private readonly encrypt: IEncrypt,
+        private readonly jwt: IJwt,
+        private readonly uuid: IUuid,
     ){};
 
-    async execute(params: any): Promise<any> {
+    async execute(params: RequestLoginUser): Promise<ResponseLoginUser> {
         const user = await this.userRepository.findByEmail(params.email);
 
         if (!user)
@@ -21,6 +25,17 @@ export class LoginUserUseCase implements IUsecase<RequestLoginUser, ResponseLogi
         if (password !== params.password)
             throw new UnauthorizedUserError();
 
-        throw new Error("Method not implemented.");
+        const token = this.jwt.generate({ email: user.email });
+        const refreshToken = this.uuid.generate();
+
+        await this.userRepository.updateRefreshtoken({
+            email: user.email,
+            refreshToken,
+        });
+
+        return {
+            accessToken: token.token,
+            refreshToken,
+        };
     }
 }
