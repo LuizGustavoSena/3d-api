e17da0157cc28d2b1016671faef9e48e34d89711
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-08-18 19:22:13 -0300

    feat: factory user usecase

--- src/main/factories/use-cases/user/create.factory.ts
+++ src/main/factories/use-cases/user/create.factory.ts
@@ -1,7 +1,7 @@
 import { CreateUserUseCase } from "../../../../data/use-cases/user/create-user.use-case";
-import { factoryEncrypt } from "../../encrypt";
-import { factoryUserRepository } from "../../repositories/mysql/user/create";
-import { factoryUuid } from "../../uuid";
+import { factoryEncrypt } from "../../encrypt/index..factory";
+import { factoryUserRepository } from "../../repositories/mysql/user/create.factory.";
+import { factoryUuid } from "../../uuid/index.factory";
 
 export function factoryCreateUserUseCase() {
     return new CreateUserUseCase(
