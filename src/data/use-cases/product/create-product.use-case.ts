e4dd355b2a4497004d0e2a73cb69426b75056828
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-08-26 18:32:59 -0300

    feat: finalizy the CreateProductUseCase

--- src/data/use-cases/product/create-product.use-case.ts
+++ src/data/use-cases/product/create-product.use-case.ts
@@ -1,21 +1,36 @@
 import { RequestCreateProduct, ResponseCreateProduct } from "../../../domain/use-cases/product/create-product.interface";
 import { IUsecase } from "../../../domain/use-cases/use-cases.interface";
+import { IProductRepository } from "../../protocols/repositories/product";
 import { IStorage } from "../../protocols/storage";
+import { IUuid } from "../../protocols/uuid";
 
 export class CreateProductUseCase implements IUsecase<RequestCreateProduct, ResponseCreateProduct> {
     constructor(
-        private readonly storage: IStorage
+        private readonly storage: IStorage,
+        private readonly productRepository: IProductRepository,
+        private readonly uuid: IUuid,
     ){};
     
     async execute(params: RequestCreateProduct): Promise<ResponseCreateProduct> {
         const { image } = params;
 
-        const imageKey = this.storage.setItem({
+        const imageKey = await this.storage.setItem({
             contentType: 'image/jpeg', 
             file: image, 
             key: `products/${params.name}`
         });
 
-        throw new Error("Method not implemented.");
+        const productId = this.uuid.generate();
+
+        await this.productRepository.create({
+            id: productId,
+            imageKey,
+            material: params.material,
+            name: params.name,
+            printingTime: params.printingTime,
+            value: params.value
+        })
+
+        return { id: productId };
     }
 }
