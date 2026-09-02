e672ddebb54badb8a967ba374ad43b4b2231a1f3
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-09-02 18:57:28 -0300

    feat: rename MYSQL_URL env name

--- src/infraestructure/validations/zod/env.ts
+++ src/infraestructure/validations/zod/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { InvalidEnvError } from "../../../domain/errors/sistem/invalid-env.error";
 
 const schema = z.object({
-    DATABASE_URL: z.string(),
+    MYSQL_URL: z.string(),
     SECRET_KEY_TOKEN: z.string(),
     SECRET_KEY_ENCRYPT: z.string(),
     SECRET_IV_ENCRYPT: z.string(),
